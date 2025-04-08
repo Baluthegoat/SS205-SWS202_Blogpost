92e39429aa3e971c2c7ae77f1d6d3760109fd7b5
Baluthegoat <02230306.cst@rub.edu.bt>
2025-04-09 00:32:05 +0600

    setup

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -1,10 +1,10 @@
 import { defineThemeConfig } from './types'
 
 export default defineThemeConfig({
-  site: 'https://nordlys.fjelloverflow.dev',
-  title: 'Nordlys',
+
+  title: 'Tashi Penjor',
   description: 'A minimal Astro blog theme',
-  author: 'FjellOverflow',
+  author: 'Tashi Penjor',
   navbarItems: [
     { label: 'Blog', href: '/posts/' },
     { label: 'Projects', href: '/projects/' },
@@ -15,7 +15,7 @@ export default defineThemeConfig({
       children: [
         { label: 'Landing page', href: '/' },
         { label: '404 page', href: '/404' },
-        { label: 'Author: FjellOverflow', href: '/authors/FjellOverflow/' },
+        { label: 'Author: Tashi Penor', href: '/authors/Tashi Penjor/' },
         { label: 'Tag: documentation', href: '/tags/documentation/' }
       ]
     }
@@ -23,7 +23,7 @@ export default defineThemeConfig({
   footerItems: [
     {
       icon: 'tabler--brand-github',
-      href: 'https://github.com/FjellOverflow/nordlys',
+      // href: 'https://github.com/FjellOverflow/nordlys',
       label: 'Github'
     },
     {
